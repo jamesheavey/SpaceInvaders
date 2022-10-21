01604c4fa4fa8ca33f2418462b8a8faeb22da783
James Heavey <james@heavey.net>
2022-10-21 03:26:45 +0100

    render assets

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SPACE INVADERS!")
 
+pygame.font.init()
+
 # Load images
 
 # Enemy ships
@@ -31,12 +33,24 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
     clock = pygame.time.Clock()
+    main_font = pygame.font.SysFont("Futura", 30)
     
     def redraw_window():
+        # add background at 0,0
         WIN.blit(BG, (0,0))
-        pygame.display.update()
+        # draw text
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        WIN.blit(level_label, (10, 10))
+        WIN.blit(lives_label, (10, 20 + level_label.get_height()))
+        WIN.blit(YELLOW_SPACE_SHIP, (200,HEIGHT - YELLOW_SPACE_SHIP.get_height()))
         
+        # update display
+        pygame.display.update()
+
     
     while(run):
         clock.tick(FPS)
